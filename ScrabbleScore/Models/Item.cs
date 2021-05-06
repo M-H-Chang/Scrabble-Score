4cd1c930f96a537e14b374eb751406f09d132bc4
Jeremy Banka <hello@jeremybanka.com>
2021-05-06 11:49:45 -0700

    💬 add dictionary for what letters score what
    Co-authored-by: Michael Burton <micbur1@gmail.com>
    Co-authored-by: Min Chang <minchangmhc@gmail.com>

--- ScrabbleScore/Models/Item.cs
+++ ScrabbleScore/Models/Item.cs
@@ -1,24 +1,49 @@
 using System.Collections.Generic;
 
-namespace ToDoList.Models
+namespace ScrabbleScore.Models
 {
-  public class Item
+  public class ScrabbleRules
   {
-    public string Description { get; set; }
-    private static List<Item> _instances = new List<Item> { };
+    // public static int A = 1;
+    // public static int
+    readonly static Dictionary<List<char>, int> LetterScores = new()
+    {
+      { new List<char> { 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' }, 1 },
+      { new List<char> { 'D', 'G' }, 2 },
+      { new List<char> { 'B', 'C', 'M', 'P' }, 3 },
+      { new List<char> { 'F', 'H', 'V', 'W', 'Y' }, 4 },
+      { new List<char> { 'K' }, 5 },
+      { new List<char> { 'J', 'X' }, 8 },
+      { new List<char> { 'Q', 'Z' }, 10 },
+    };
+  }
+  public class Word
+  {
+    // string[] valueOne = { "A", "E", "I", "O", "U", "L", "N", "R", "S", "T" };
+    // string[] valueTwo = { "D", "G" };
+    // string[] valueThree = { "B", "C", "M", "P" };
+    // string[] valueFour = { "F", "H", "V", "W", "Y" };
+    // string[] valueFive = { "K" };
+    // string[] valueEight = { "J", "X" };
+    // string[] valueTen = { "Q", "Z" };
 
-    public Item(string description)
+    public static int GetLetterValue(string input)
     {
-      Description = description;
-      _instances.Add(this);
-    }
-    public static List<Item> GetAll()
-    {
-      return _instances;
-    }
-    public static void ClearAll()
-    {
-      _instances.Clear();
+      string userInput = input;
+      for (int index = 0; index < userInput.Length; index++)
+      {
+        string letter = userInput.Substring(index, 1);
+        if (letter == "A")
+        {
+          return 1;
+        }
+      }
     }
   }
 }
+
+// letter
+// scrabbleDictionary[letter] -> int
+// scrabbleDictionary["A"] -> 1
+
+
